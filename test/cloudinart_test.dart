edc7eb7278d5cc0e128946278ca023c37eef6412
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-10-11 15:58:42 +0300

    Add project files

---
+++ test/cloudinart_test.dart
@@ -0,0 +1,16 @@
+import 'package:cloudinary_dart/cloudinary.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Cloud Config Tests', () {
+    test('Should successfully initializes Cloudinary object using valid url',
+            () {
+          Cloudinary cloudinary = Cloudinary.withStringUrl(
+              "cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com");
+          assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
+          assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
+          assert("3Sf3FAdasa2easdFGDS3afADFS2" ==
+              cloudinary.config.cloudConfig.apiSecret);
+        });
+  });
+}
