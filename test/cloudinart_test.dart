cd3691570cf062816c987c02133ee31819d927a2
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-10-20 14:09:05 +0200

    Fix code indentation

--- test/cloudinart_test.dart
+++ test/cloudinart_test.dart
@@ -4,13 +4,13 @@ import 'package:test/scaffolding.dart';
 void main() {
   group('Cloud Config Tests', () {
     test('Should successfully initializes Cloudinary object using valid url',
-            () {
-          Cloudinary cloudinary = Cloudinary.withStringUrl(
-              "cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com");
-          assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
-          assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
-          assert("3Sf3FAdasa2easdFGDS3afADFS2" ==
-              cloudinary.config.cloudConfig.apiSecret);
-        });
+        () {
+      Cloudinary cloudinary = Cloudinary.withStringUrl(
+          "cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com");
+      assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
+      assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
+      assert("3Sf3FAdasa2easdFGDS3afADFS2" ==
+          cloudinary.config.cloudConfig.apiSecret);
+    });
   });
 }
