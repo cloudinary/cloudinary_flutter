a1a52f7dc09d379cf090f2a216bfe60ebf351244
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-11-14 11:59:05 +0200

    Update `CldImage` widget

--- lib/cld_image.dart
+++ lib/cld_image.dart
@@ -1,11 +1,26 @@
+import 'package:cloudinary_dart/asset/cld_image.dart';
 import 'package:cloudinary_dart/transformation/transformation.dart';
 import 'package:flutter/widgets.dart';
 import 'cloudinary_context.dart';
 
-class CldImage extends StatefulWidget {
-  final String publicId;
-  final Transformation? transformation;
-  const CldImage(this.publicId, {super.key, this.transformation});
+class CldImageWidget extends StatefulWidget {
+  late final CldImage image;
+  CldImageWidget(String publicId,
+      {super.key,
+      String? version,
+      String? extension,
+      String? urlSuffix,
+      String? assetType,
+      String? deliveryType,
+      Transformation? transformation}) {
+    image = CloudinaryContext.cloudinary.image(publicId);
+    ((version != null) ? image.version(version) : null);
+    ((extension != null) ? image.extension(extension) : null);
+    ((urlSuffix != null) ? image.urlSuffix(urlSuffix) : null);
+    ((assetType != null) ? image.assetType(assetType) : null);
+    ((deliveryType != null) ? image.deliveryType(deliveryType) : null);
+    ((transformation != null) ? image.transformation(transformation) : null);
+  }
 
   @override
   State<StatefulWidget> createState() {
@@ -13,11 +28,9 @@ class CldImage extends StatefulWidget {
   }
 }
 
-class _CldImageState extends State<CldImage> {
+class _CldImageState extends State<CldImageWidget> {
   @override
   Widget build(BuildContext context) {
-    return Image.network((CloudinaryContext.cloudinary.image(widget.publicId)
-          ..transformation(widget.transformation ?? Transformation()))
-        .toString());
+    return Image.network(widget.image.toString());
   }
 }
