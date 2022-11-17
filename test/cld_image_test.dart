198c62b34b72d614418324051449b24189ff6091
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-11-17 10:38:30 +0200

    Upgrade `cloudinary_dart` to 0.0.7

---
+++ test/cld_image_test.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:cloudinary_dart/cloudinary.dart';
+import 'package:cloudinary_dart/transformation/resize/resize.dart';
+import 'package:cloudinary_dart/transformation/transformation.dart';
+import 'package:cloudinary_flutter/cld_image.dart';
+import 'package:cloudinary_flutter/cloudinary_context.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  CloudinaryContext.cloudinary = Cloudinary.fromCloudName(cloudName: 'demo');
+  CloudinaryContext.cloudinary.config.urlConfig.analytics = false;
+  setUpAll(() => HttpOverrides.global = null);
+  testWidgets('Test CldImageWidget has valid url', (widgetTester) async {
+    var widget = CldImageWidget(publicId: 'dog');
+    await widgetTester.pumpWidget(widget);
+
+    final imageFinder = find.image(
+        NetworkImage('https://res.cloudinary.com/demo/image/upload/dog'));
+    expect(imageFinder, findsOneWidget);
+  });
+
+  testWidgets('Test CldImageWidget with version has valid url',
+      (widgetTester) async {
+    await widgetTester.pumpWidget(CldImageWidget(
+      publicId: ('sample'),
+      version: "1",
+    ));
+    final imageFinder = find.image(
+        NetworkImage('https://res.cloudinary.com/demo/image/upload/v1/sample'));
+    expect(imageFinder, findsOneWidget);
+  });
+
+  testWidgets('Test CldImageWidget with version and urlsuffix has valid url',
+      (widgetTester) async {
+    await widgetTester.pumpWidget(CldImageWidget(
+      publicId: ('sample'),
+      version: "1",
+      urlSuffix: 'test',
+    ));
+
+    final imageFinder = find.image(
+        NetworkImage('https://res.cloudinary.com/demo/images/v1/sample/test'));
+    expect(imageFinder, findsOneWidget);
+  });
+
+  testWidgets(
+      'Test CldImageWidget with version and transformation has valid url',
+      (widgetTester) async {
+    await widgetTester.pumpWidget(CldImageWidget(
+      publicId: ('sample'),
+      version: "1",
+      urlSuffix: 'test',
+      transformation: Transformation()..resize(Resize.scale()..width(500)),
+    ));
+
+    final imageFinder = find.image(NetworkImage(
+        'https://res.cloudinary.com/demo/images/c_scale,w_500/v1/sample/test'));
+    expect(imageFinder, findsOneWidget);
+  });
+}
