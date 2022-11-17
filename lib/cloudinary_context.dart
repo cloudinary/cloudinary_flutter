a4d208aeb5e3939d1c35f46b9196a48b741080e6
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-11-17 07:47:25 +0200

    Upgrade cloudinary_dart to 0.0.7

--- lib/cloudinary_context.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cloudinary_dart/cloudinary.dart';
-
-class CloudinaryContext {
-  static Cloudinary cloudinary = Cloudinary();
-}
--- test/cloudinart_test.dart
+++ test/cloudinart_test.dart
@@ -5,7 +5,7 @@ void main() {
   group('Cloud Config Tests', () {
     test('Should successfully initializes Cloudinary object using valid url',
         () {
-      Cloudinary cloudinary = Cloudinary.withStringUrl(
+      Cloudinary cloudinary = Cloudinary.fromStringUrl(
           "cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com");
       assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
       assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
