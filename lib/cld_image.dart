313a73d40eb293713129613d59ea341ee5237883
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2022-11-30 12:03:53 +0200

    Expose image features to CldImageWidget

--- lib/cld_image.dart
+++ lib/cld_image.dart
@@ -3,8 +3,9 @@ import 'package:cloudinary_dart/transformation/transformation.dart';
 import 'package:flutter/widgets.dart';
 import 'cloudinary_context.dart';
 
-class CldImageWidget extends StatefulWidget {
-  late final CldImage image;
+class CldImageWidget extends Image {
+  late final CldImage cldImage;
+
   CldImageWidget(
       {required String publicId,
       super.key,
@@ -13,18 +14,56 @@ class CldImageWidget extends StatefulWidget {
       String? urlSuffix,
       String? assetType,
       String? deliveryType,
-      Transformation? transformation}) {
-    image = CloudinaryContext.cloudinary.image(publicId);
-    ((version != null) ? image.version(version) : null);
-    ((extension != null) ? image.extension(extension) : null);
-    ((urlSuffix != null) ? image.urlSuffix(urlSuffix) : null);
-    ((assetType != null) ? image.assetType(assetType) : null);
-    ((deliveryType != null) ? image.deliveryType(deliveryType) : null);
-    ((transformation != null) ? image.transformation(transformation) : null);
+      Transformation? transformation,
+      ImageFrameBuilder? frameBuilder,
+      ImageLoadingBuilder? loadingBuilder,
+      ImageErrorWidgetBuilder? errorBuilder,
+      String? semanticLabel,
+      bool excludeFromSemantics = false,
+      double? width,
+      double? height,
+      Color? color,
+      Animation<double>? opacity,
+      BlendMode? colorBlendMode,
+      BoxFit? fit,
+      AlignmentGeometry alignment = Alignment.center,
+      ImageRepeat repeat = ImageRepeat.noRepeat,
+      Rect? centerSlice,
+      bool matchTextDirection = false,
+      bool gaplessPlayback = false,
+      bool isAntiAlias = false,
+      FilterQuality filterQuality = FilterQuality.low})
+      : super(
+            image: NetworkImage(''),
+            frameBuilder: frameBuilder,
+            loadingBuilder: loadingBuilder,
+            errorBuilder: errorBuilder,
+            semanticLabel: semanticLabel,
+            excludeFromSemantics: excludeFromSemantics,
+            width: width,
+            height: height,
+            color: color,
+            opacity: opacity,
+            colorBlendMode: colorBlendMode,
+            fit: fit,
+            alignment: alignment,
+            repeat: repeat,
+            centerSlice: centerSlice,
+            matchTextDirection: matchTextDirection,
+            gaplessPlayback: gaplessPlayback,
+            isAntiAlias: isAntiAlias,
+            filterQuality: filterQuality) {
+    cldImage = CloudinaryContext.cloudinary.image(publicId);
+    ((version != null) ? cldImage.version(version) : null);
+    ((extension != null) ? cldImage.extension(extension) : null);
+    ((urlSuffix != null) ? cldImage.urlSuffix(urlSuffix) : null);
+    ((assetType != null) ? cldImage.assetType(assetType) : null);
+    ((deliveryType != null) ? cldImage.deliveryType(deliveryType) : null);
+    ((transformation != null) ? cldImage.transformation(transformation) : null);
   }
 
   @override
-  State<StatefulWidget> createState() {
+  State<Image> createState() {
     return _CldImageState();
   }
 }
@@ -32,6 +71,6 @@ class CldImageWidget extends StatefulWidget {
 class _CldImageState extends State<CldImageWidget> {
   @override
   Widget build(BuildContext context) {
-    return Image.network(widget.image.toString());
+    return Image.network(widget.cldImage.toString());
   }
 }
