d1760e3d2ffd0b9f7ddf8bd17c0354c7f99294f3
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-03-09 15:43:06 +0200

    Add URLCache for images

---
+++ lib/image/cld_image.dart
@@ -0,0 +1,115 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloudinary_dart/asset/cld_image.dart';
+import 'package:cloudinary_dart/transformation/transformation.dart';
+import 'package:cloudinary_flutter/image/no_disk_cache_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+import '../cloudinary_context.dart';
+import 'cld_image_widget_configuration.dart';
+
+/// A widget that displays an image.
+/// A constructor with multiple attributes provided for the various ways that an image can be
+/// The widget is meant to display an image from the Cloudinary's cloud
+/// The [publicId] field should not be null and must be provided.
+class CldImageWidget extends CachedNetworkImage {
+  /// Cloudinary image object
+  /// This object holds all of Cloudinary's attributes.
+  late final CldImage cldImage;
+  final CldImageWidgetConfiguration? configuration;
+
+  CldImageWidget(
+      {required String publicId,
+      super.key,
+      this.configuration,
+      String? version,
+      String? extension,
+      String? urlSuffix,
+      String? assetType,
+      String? deliveryType,
+      Transformation? transformation,
+      Map<String, String>? httpHeaders,
+      ImageWidgetBuilder? imageBuilder,
+      PlaceholderWidgetBuilder? placeholder,
+      LoadingErrorWidgetBuilder? errorBuilder,
+      Duration? placeholderFadeInDuration,
+      int? memCacheWidth,
+      int? memCacheHeight,
+      String? cacheKey,
+      int? maxWidthDiskCache,
+      int? maxHeightDiskCache,
+      double? width,
+      double? height,
+      Color? color,
+      BlendMode? colorBlendMode,
+      BoxFit? fit,
+      ImageRepeat repeat = ImageRepeat.noRepeat,
+      FilterQuality filterQuality = FilterQuality.low,
+      bool matchTextDirection = false})
+      : super(
+            imageUrl: '',
+            httpHeaders: httpHeaders,
+            imageBuilder: imageBuilder,
+            placeholder: placeholder,
+            errorWidget: errorBuilder,
+            width: width,
+            height: height,
+            color: color,
+            filterQuality: filterQuality,
+            colorBlendMode: colorBlendMode,
+            placeholderFadeInDuration: placeholderFadeInDuration,
+            memCacheWidth: memCacheWidth,
+            memCacheHeight: memCacheHeight,
+            cacheKey: cacheKey,
+            maxWidthDiskCache: maxWidthDiskCache,
+            maxHeightDiskCache: maxHeightDiskCache,
+            fit: fit,
+            repeat: repeat,
+            matchTextDirection: matchTextDirection) {
+    cldImage = CloudinaryContext.cloudinary.image(publicId);
+    if (version != null) {
+      cldImage.version(version);
+    }
+    if (extension != null) {
+      cldImage.extension(extension);
+    }
+    if (urlSuffix != null) {
+      cldImage.urlSuffix(urlSuffix);
+    }
+    if (assetType != null) {
+      cldImage.assetType(assetType);
+    }
+    if (deliveryType != null) {
+      cldImage.deliveryType(deliveryType);
+    }
+    if (transformation != null) {
+      cldImage.transformation(transformation);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var cacheManager = (configuration != null && !configuration!.cache) ? NoDiskCacheManager.instance : DefaultCacheManager();
+      return CachedNetworkImage(
+          imageUrl: cldImage.toString(),
+          cacheManager: cacheManager,
+          httpHeaders: super.httpHeaders,
+          imageBuilder: super.imageBuilder,
+          placeholder: super.placeholder,
+          errorWidget: super.errorWidget,
+          width: super.width,
+          height: super.height,
+          color: super.color,
+          filterQuality: super.filterQuality,
+          colorBlendMode: super.colorBlendMode,
+          placeholderFadeInDuration: super.placeholderFadeInDuration,
+          memCacheWidth: super.memCacheWidth,
+          memCacheHeight: super.memCacheHeight,
+          cacheKey: super.cacheKey,
+          maxWidthDiskCache: super.maxWidthDiskCache,
+          maxHeightDiskCache: super.maxHeightDiskCache,
+          fit: super.fit,
+          repeat: super.repeat,
+          matchTextDirection: super.matchTextDirection);
+  }
+}
--- test/cld_image_test.dart
+++ test/cld_image_test.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloudinary_dart/cloudinary.dart';
 import 'package:cloudinary_dart/transformation/resize/resize.dart';
 import 'package:cloudinary_dart/transformation/transformation.dart';
-import 'package:cloudinary_flutter/cld_image.dart';
 import 'package:cloudinary_flutter/cloudinary_context.dart';
+import 'package:cloudinary_flutter/image/cld_image.dart';
+import 'package:cloudinary_flutter/image/cld_image_widget_configuration.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,12 +13,13 @@ void main() {
   CloudinaryContext.cloudinary = Cloudinary.fromCloudName(cloudName: 'demo');
   CloudinaryContext.cloudinary.config.urlConfig.analytics = false;
   setUpAll(() => HttpOverrides.global = null);
+
   testWidgets('Test CldImageWidget has valid url', (widgetTester) async {
     var widget = CldImageWidget(publicId: 'dog', width: 500, height: 100);
     await widgetTester.pumpWidget(widget);
 
-    final imageFinder = find.image(
-        NetworkImage('https://res.cloudinary.com/demo/image/upload/dog'));
+    final imageFinder = find.image(CachedNetworkImageProvider(
+        'https://res.cloudinary.com/demo/image/upload/dog'));
     expect(imageFinder, findsOneWidget);
   });
 
@@ -27,11 +29,10 @@ void main() {
       publicId: ('sample'),
       version: "1",
     ));
-    final imageFinder = find.image(
-        NetworkImage('https://res.cloudinary.com/demo/image/upload/v1/sample'));
+    final imageFinder = find.image(CachedNetworkImageProvider(
+        'https://res.cloudinary.com/demo/image/upload/v1/sample'));
     expect(imageFinder, findsOneWidget);
   });
-
   testWidgets('Test CldImageWidget with version and urlsuffix has valid url',
       (widgetTester) async {
     await widgetTester.pumpWidget(CldImageWidget(
@@ -39,12 +40,10 @@ void main() {
       version: "1",
       urlSuffix: 'test',
     ));
-
-    final imageFinder = find.image(
-        NetworkImage('https://res.cloudinary.com/demo/images/v1/sample/test'));
+    final imageFinder = find.image(CachedNetworkImageProvider(
+        'https://res.cloudinary.com/demo/images/v1/sample/test'));
     expect(imageFinder, findsOneWidget);
   });
-
   testWidgets(
       'Test CldImageWidget with version and transformation has valid url',
       (widgetTester) async {
@@ -54,9 +53,20 @@ void main() {
       urlSuffix: 'test',
       transformation: Transformation()..resize(Resize.scale()..width(500)),
     ));
-
-    final imageFinder = find.image(NetworkImage(
+    final imageFinder = find.image(CachedNetworkImageProvider(
         'https://res.cloudinary.com/demo/images/c_scale,w_500/v1/sample/test'));
+    expect(imageFinder, findsOneWidget);
+  });
+  testWidgets('Test CldImageWidget without cache', (widgetTester) async {
+    var widget = CldImageWidget(
+        publicId: 'dog',
+        configuration: CldImageWidgetConfiguration(cache: false),
+        width: 500,
+        height: 100);
+    await widgetTester.pumpWidget(widget);
+
+    final imageFinder = find.image(CachedNetworkImageProvider(
+        'https://res.cloudinary.com/demo/image/upload/dog'));
     expect(imageFinder, findsOneWidget);
   });
 }
