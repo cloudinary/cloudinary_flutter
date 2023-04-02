0b9273b71b644b4a2a5512f7542d141da9967a76
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-04-02 11:22:24 +0300

    Add missing doc strings

--- lib/cloudinary_context.dart
+++ lib/cloudinary_context.dart
@@ -1,5 +1,6 @@
 import 'package:cloudinary_url_gen/cloudinary.dart';
 
+/// Helper class to help users define the Cloudinary object to be used with their [CLDImageWidget]
 class CloudinaryContext {
   static Cloudinary cloudinary = Cloudinary();
 }
