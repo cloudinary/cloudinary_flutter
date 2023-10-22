8ec625a22e2b93a95df9a72801bcaa949a98b781
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2023-10-22 12:32:03 +0300

    Fix formatting

--- lib/cloudinary_context.dart
+++ lib/cloudinary_context.dart
@@ -2,6 +2,7 @@ import 'package:cloudinary_flutter/image/cld_image.dart';
 import 'package:cloudinary_url_gen/cloudinary.dart';
 
 @Deprecated('Please use CloudinaryObject instead')
+
 /// Helper class to help users define the Cloudinary object to be used with their [CldImageWidget]
 class CloudinaryContext {
   static Cloudinary cloudinary = Cloudinary();
