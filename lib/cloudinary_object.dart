c3090605ac2ac0a4e47f1fabe1beea607d7e46c2
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-12-03 07:27:25 +0200

    Add `user_agent` support

--- lib/cloudinary_object.dart
+++ lib/cloudinary_object.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:cloudinary_url_gen/analytics.dart';
 import 'package:cloudinary_url_gen/cloudinary.dart';
 
+final sdkVersion = '1.1.0';
+
 /// Helper class to help users define the Cloudinary object to be used with their widgets
 class CloudinaryObject extends Cloudinary {
   CloudinaryObject.fromCloudName({required String cloudName})
@@ -10,6 +12,7 @@ class CloudinaryObject extends Cloudinary {
     var techVersion = Platform.version.split(" ")[0];
     Analytics analytics =
         Analytics.fromParameters('O', sdkVersion, techVersion, _getOsVersion());
+    Cloudinary.userAgent = 'CloudinaryFlutter/$sdkVersion';
     super.setAnalytics(analytics);
   }
 
--- test/cloudinary_test.dart
+++ test/cloudinary_test.dart
@@ -1,3 +1,4 @@
+import 'package:cloudinary_flutter/cloudinary_object.dart';
 import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:test/scaffolding.dart';
 
@@ -11,6 +12,11 @@ void main() {
       assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
       assert("3Sf3FAdasa2easdFGDS3afADFS2" ==
           cloudinary.config.cloudConfig.apiSecret);
+    });
+
+    test('Should succesfully set user agent', () {
+      CloudinaryObject.fromCloudName(cloudName: "test");
+      assert(Cloudinary.userAgent.contains('CloudinaryFlutter'));
     });
   });
 }
