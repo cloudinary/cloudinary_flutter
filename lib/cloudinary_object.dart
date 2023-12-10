336af1b082387374ee7d29c72b527f2748f5f6d6
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2023-12-10 16:49:14 +0200

    Fix analytics token for web apps

--- lib/cloudinary_object.dart
+++ lib/cloudinary_object.dart
@@ -1,6 +1,4 @@
-import 'dart:io';
-
-import 'package:cloudinary_url_gen/analytics.dart';
+import 'package:cloudinary_url_gen/analytics/analytics.dart';
 import 'package:cloudinary_url_gen/cloudinary.dart';
 
 final sdkVersion = '1.1.0';
@@ -9,17 +7,9 @@ final sdkVersion = '1.1.0';
 class CloudinaryObject extends Cloudinary {
   CloudinaryObject.fromCloudName({required String cloudName})
       : super.fromCloudName(cloudName: cloudName) {
-    var techVersion = Platform.version.split(" ")[0];
-    Analytics analytics =
-        Analytics.fromParameters('O', sdkVersion, techVersion, _getOsVersion());
     Cloudinary.userAgent = 'CloudinaryFlutter/$sdkVersion';
+    Analytics analytics =
+    Analytics.fromParameters(sdk: 'O', version: sdkVersion);
     super.setAnalytics(analytics);
-  }
-
-  String _getOsVersion() {
-    if (Platform.isAndroid || Platform.isIOS) {
-      return Platform.operatingSystemVersion;
-    }
-    return 'AA';
   }
 }
