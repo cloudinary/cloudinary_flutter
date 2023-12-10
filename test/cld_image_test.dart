bbdc4f4add7d5e6a5222be111575013533e14793
Adi Mizrahi <adi.mizrahi@cloudinary.com>
2023-12-10 15:30:05 +0200

    Fix tests

--- test/cld_image_test.dart
+++ test/cld_image_test.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloudinary_url_gen/analytics.dart';
+import 'package:cloudinary_url_gen/analytics/analytics.dart';
 import 'package:cloudinary_url_gen/transformation/resize/resize.dart';
 import 'package:cloudinary_url_gen/transformation/transformation.dart';
 import 'package:cloudinary_flutter/cloudinary_object.dart';
@@ -77,14 +77,14 @@ void main() {
 
   testWidgets('Test CldImageWidget has valid url with analytics',
       (widgetTester) async {
-    cloudinary.analytics = Analytics.fromParameters("O", "1.0.0", "39.1", "");
+    cloudinary.analytics = Analytics.fromParameters(sdk: 'O', version: '1.0.0', techVersion: '39.1', osType: 'Z', osVersion: '0');
     cloudinary.config.urlConfig.analytics = true;
     var widget = CldImageWidget(
         cloudinary: cloudinary, publicId: 'dog', width: 500, height: 100);
     await widgetTester.pumpWidget(widget);
 
     final imageFinder = find.image(CachedNetworkImageProvider(
-        'https://res.cloudinary.com/demo/image/upload/dog?_a=CAOAABCLZAA0'));
+        'https://res.cloudinary.com/demo/image/upload/dog?_a=DAOAABCLZAA0'));
     expect(imageFinder, findsOneWidget);
   });
 }
